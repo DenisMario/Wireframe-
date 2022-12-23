e9b2c9554fe5cb252419b742c447bac62a98aa19
DenisMario <120132656+DenisMario@users.noreply.github.com>
2022-12-23 10:22:04 -0700

    Update Wireframe.py

--- Wireframe.py
+++ Wireframe.py
@@ -9,7 +9,9 @@ ax = fig.add_subplot(1,1,1, projection = '3d')
 x,y,z = axes3d.get_test_data(0.05)
 ax.plot_wireframe(x,y,z,rstride=10,cstride=10)
 
-ax.set_title('Wireframe example')
-
+ax.set_title('Wireframe example',color='orange')
+ax.set_ylabel('Y-axis',color ='pink')
+ax.set_xlabel('X-axis',color ='red')
+ax.set_zlabel('Z-axis',color='cyan')
 
 plt.show()
